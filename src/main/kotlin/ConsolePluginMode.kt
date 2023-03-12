4eec6d5b7d7904bd75fb8cbbc80356f5820b42c7
cssxsh <cssxsh@gmail.com>
2023-03-12 19:45:02 +0800

    fix: Plugin errorLog

--- src/main/kotlin/ConsolePluginMode.kt
+++ src/main/kotlin/ConsolePluginMode.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019-2021 Mamoe Technologies and contributors.
+ * Copyright 2019-2023 Mamoe Technologies and contributors.
  *
  * 此源代码的使用受 GNU AFFERO GENERAL PUBLIC LICENSE version 3 许可证的约束, 可以在以下链接找到该许可证.
  * Use of this source code is governed by the GNU AGPLv3 license that can be found through the following link.
@@ -42,6 +42,10 @@ internal object MiraiSilkConverterConsolePlugin : KotlinPlugin(JvmPluginDescript
                 object : AudioToSilkCoder(exec) {
                     override fun cmdLine(nextLine: String?) {
                         logger.verbose { nextLine ?: "null" }
+                    }
+
+                    override fun errorLog(throwable: Throwable?) {
+                        logger.error(throwable)
                     }
                 }
             }
